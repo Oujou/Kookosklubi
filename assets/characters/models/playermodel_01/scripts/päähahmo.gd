167ffad3cbd3f587515290b736b4f5f36810ab01
Juuso Tontti <juuso.j.tontti@gmail.com>
2023-11-16 15:44:05 +0200

    Päähahmolle animoitu placeholder

--- assets/characters/models/playermodel_01/scripts/päähahmo.gd
+++ assets/characters/models/playermodel_01/scripts/päähahmo.gd
@@ -1,12 +1,13 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
+const SPEED = 150.0
+const JUMP_VELOCITY = -400.0
+
+@onready var sprite = $Päähahmo_model
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -25,4 +26,18 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	if direction != 0:
+		sprite.flip_h = (direction == -1)
+
 	move_and_slide()
+	
+	update_animations(direction)
+	
+func update_animations(direction):
+	if is_on_floor():
+		if direction == 0:
+			sprite.play("Idle")
+		else:
+			sprite.play("Run_right")
+	else:
+		pass
