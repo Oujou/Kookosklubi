d1ec96047419921ca4d37dfa8d297e7cedb1f641
Juuso Tontti <juuso.j.tontti@gmail.com>
2023-11-18 01:24:25 +0200

    Luoti collision

--- assets/characters/models/playermodel_01/scripts/päähahmo.gd
+++ assets/characters/models/playermodel_01/scripts/päähahmo.gd
@@ -1,7 +1,13 @@
 extends CharacterBody2D
 @onready var screensize = get_viewport_rect().size
 @export var Area2d_Luoti : PackedScene = preload("res://scenes/area_2d_luoti.tscn")
+
+var on_ladder : bool = false
+
+# Character speed
 const SPEED = 150.0
+
+# How high main character can jump negative means upwards, positive downwards
 const JUMP_VELOCITY = -400.0
 
 @onready var sprite = $Päähahmo_model
@@ -11,14 +17,16 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
+	#print(on_ladder)
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("Up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+	if Input.is_action_just_pressed("Up"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("Left", "Right")
@@ -39,14 +47,28 @@ func _physics_process(delta):
 
 func update_animations(direction):
 	if is_on_floor():
+		# If player is not moving play idle animation from päähahmo.tscn
 		if direction == 0:
+			# Play Idle animation from päähahmo.tscn
 			sprite.play("Idle")
 		else:
+			# Play Run_right animation from päähahmo.tscn
 			sprite.play("Run_right")
 	else:
 		pass
 
 func shoot():
+	# Spawn new scene from area_2d_luoti 
 	var b = Area2d_Luoti.instantiate()
+	# 
 	owner.add_child(b)
+	
+	# Move bullet spawnpoint to near our character which is set up in Päähahmo_container
 	b.transform = $bulletSpawnPoint.global_transform
+
+
+func _on_ladder_check_body_entered(body):
+	on_ladder = true
+
+func _on_ladder_check_body_exited(body):
+	on_ladder = false
--- assets/graphics/bullets/Area2D_ Luoti.gd
+++ assets/graphics/bullets/Area2D_ Luoti.gd
@@ -1,13 +1,22 @@
 extends Area2D
 
+#Projectile speed
 var speed = 750
 
 func _physics_process(delta):
 	position += transform.x * speed * delta
 
 func _on_body_entered(body):
+	# If projectile enters into mobs categorized hitbox
 	if body.is_in_group("mobs"):
+		# Destroy projectile
 		body.queue_free()
+	# Tilesets are treated as bodies so if projectile hits tileset then destroy
+	if body.name == "TileMap" or body.name == "tilesetti2":
+		# Destroy projectile
+		queue_free()
 
+# If projectile exits screen, destroy
 func _on_visible_on_screen_notifier_2d_screen_exited():
+	# Destroy
 	queue_free()
